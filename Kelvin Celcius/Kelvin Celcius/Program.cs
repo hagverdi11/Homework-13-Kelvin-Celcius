860e8a4c15f391a8662cc39770eb2c69fffe9979
hagverdi11 <hagverdiam@code.edu.az>
2022-07-08 01:41:03 +0400

    Added new homework table

---
+++ Kelvin Celcius/Kelvin Celcius/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Implicit_Explicit_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celcius celcius = new Celcius(-100);
+            Kelvin kelvin = celcius;
+            Console.WriteLine(kelvin.Kl);
+
+
+     
+        
+        
+        }
+    }
+
+
+    public class Kelvin
+    {
+        public double Kl { get; set; }
+
+        public Kelvin(double kl)
+        {
+            Kl = kl;
+        }
+    }
+
+
+
+    public class Celcius
+    {
+        public double Cl { get; set; }
+
+        public Celcius(double cl)
+        {
+            Cl=cl;
+
+        }
+     
+        
+        public static implicit operator Kelvin (Celcius celcius)
+        {
+            return new Kelvin(celcius.Cl + 273.15);
+        }
+        
+    }
+
+}
+
+
+
+
